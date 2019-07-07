6d7348b11fca4c375d3cc67bebec3b9545092c9c
Kiryl Matusevich <iamkirylm@gmail.com>
2019-07-07 16:29:18 +0200

    Updates

---
+++ precache-manifest.758d0d6224eb8b965708fd988cdef6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb12893899985d761b8a3d5106002ef",
+    "url": "/cryptoRate/index.html"
+  },
+  {
+    "revision": "b0501e871313ccca1bca",
+    "url": "/cryptoRate/static/css/main.c40719d8.chunk.css"
+  },
+  {
+    "revision": "d6e2fc731ff4a58142cb",
+    "url": "/cryptoRate/static/js/2.74fcc040.chunk.js"
+  },
+  {
+    "revision": "b0501e871313ccca1bca",
+    "url": "/cryptoRate/static/js/main.b0a47a53.chunk.js"
+  },
+  {
+    "revision": "e217f02139961fda8324",
+    "url": "/cryptoRate/static/js/runtime~main.72be8207.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cryptoRate/static/media/logo.5d5d9eef.svg"
+  }
+]);
